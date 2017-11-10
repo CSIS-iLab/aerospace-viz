9e2a60187ce77d073ade187e3161460e28db0b28
jnschrag <jnschrag@gmail.com>
2017-11-10 14:04:08 -0500

    Create stacked area chart template that supports multiple datasets running from Google Sheets

---
+++ templates/hc-area-multiple/js/area.js
@@ -0,0 +1,118 @@
+$(function() {
+
+  var data = {}
+  var datasets
+  var seriesData = []
+
+  Highcharts.data({
+    googleSpreadsheetKey: '1GKIk9j34dFOIkPj-7Ao-tz0d1sFpre5FbczUl4vGusM',
+      googleSpreadsheetWorksheet: 1,
+      switchRowsAndColumns: true,
+      parsed: function(columns) {
+        $.each(columns, function (i, code) {
+          if ( i == 0 ) {
+            return
+          }
+
+          data[code[0]] = data[code[0]] || {}
+          data[code[0]][code[1]] = data[code[0]][code[1]] || {
+            name: code[1],
+            data: []
+          }
+
+          data[code[0]][code[1]].data.push({
+            name: code[0],
+            x: code[2],
+            y: code[3],
+            label: code[1]
+          })
+
+        })
+
+        datasets = Object.keys(data)
+
+        // Convert object to array - we no longer need the keys
+        var dataArray = $.map(data, function(value, index) {
+            return [value];
+        });
+
+        // Convert each series into an array
+        dataArray.forEach(function(value) {
+          var series = $.map(value, function(value2, index2) {
+              return [value2];
+          });
+          seriesData.push(series)
+        })
+
+        populateSelect()
+        renderChart(seriesData[0])
+
+      }
+  })
+
+  function populateSelect() {
+    var options = '';
+    $.each(datasets, function(i, dataset) {
+      options += '<option value="'+ i + '">' + dataset + '</option>';
+    })
+    $('.datasets').append(options);
+
+    // Destroy & redraw chart so we get smooth animation when switching datasets.
+    $('.datasets').on('change', function() {
+      var chart = $('#hcContainer').highcharts()
+      chart.destroy()
+      renderChart(seriesData[this.value])
+    })
+  }
+
+  function renderChart(data) {
+    $('#hcContainer').highcharts({
+      // General Chart Options
+      chart: {
+        zoomType: 'x',
+        type: 'area'
+      },
+      // Chart Title and Subtitle
+      title: {
+        text: "Interactive Title"
+      },
+      subtitle: {
+        text: "Click and drag to zoom in"
+      },
+      // Credits
+      credits: {
+        enabled: true,
+        href: false,
+        text: "CSIS Aerospace Security | Source: NAME"
+      },
+      // Chart Legend
+      legend: {
+        align: 'center',
+        verticalAlign: 'bottom',
+        layout: 'horizontal'
+      },
+      // X Axis
+      xAxis: {
+        allowDecimals: false
+      },
+      // Y Axis
+      yAxis: {
+        title: { 
+          text: "Y Axis Title"
+        },
+      },
+      series: data,
+      // Additional Plot Options
+      plotOptions:
+      {
+        area: {
+          stacking: "normal",
+          marker: {
+            enabled: false,
+            symbol: "circle"
+          }
+        }
+      }
+    });
+  }
+});
