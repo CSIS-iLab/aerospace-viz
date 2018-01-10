bfc63c3f3817b614b34efee1e35b45ead7a8a472
junhwang77 <junhwang77@hotmail.com>
2018-01-10 11:16:50 -0500

    Add LaunchesPerYear highchart

---
+++ LaunchesPerYear/js/scatter.js
@@ -0,0 +1,58 @@
+
+$(function() {
+  $('#hcContainer').highcharts({
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '1p22ea2G3K8ikFoowmSOMK4KCLSdstAFir3qbgxawJCw',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    // Chart Title and Subtitle
+    title: {
+      text: "Number of Launches per Year"
+    },
+    /*
+    subtitle: {
+      text: "Click and drag to zoom in"
+    },*/
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS Aerospace Security | Source: NAME"
+    },
+    // Chart Legend
+    legend: {
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // X Axis
+    xAxis: {
+      title: {
+        text: "Year"
+      }
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: "Number of Launches"
+      }
+    },
+    // Additional Plot Options
+    plotOptions:
+    {
+      scatter: {
+        marker: {
+          enabled: true,
+          symbol: "circle",
+          radius: 5
+        }
+      }
+    }
+  });
+});
