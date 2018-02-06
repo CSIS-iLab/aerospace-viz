c5a1b7b322f161709562672a7d119ad97850ca8d
krjohnson <kaitlyn.johnson626@gmail.com>
2018-02-06 14:36:27 -0500

    New Bar Chart on Air Force budget

---
+++ FY19_USAF/js/bar.js
@@ -0,0 +1,57 @@
+$(function() {
+  $('#hcContainer').highcharts({
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '18MclhcrGMnlHgdMn2npk2fkgxED9Q-jY8nHbtdxqpJc',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      zoomType: 'x',
+      type: 'column'
+    },
+    // Chart Title and Subtitle
+    title: {
+      text: "United States Air Force Budget by Weapon System"
+    },
+    subtitle: {
+      text: "FY 2018 Request and FY 2019 Estimates of Selected Programs"
+    },
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS Aerospace Security | Source: US Air Force Budget Documents"
+    },
+    // Chart Legend
+    legend: {
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: "Then Year Dollars, in Millions"
+      },
+    },
+    // Tooltip
+    /*
+    tooltip: {
+        formatter: function () {
+            return '<span style="color:' + this.series.color + '">● </span><b>' + this.point.series.name + '</b><br> x: ' + this.x + ' y: ' + this.y + '<br><i>x: ' + this.x + ' y: ' + this.y + '</i><br><b>x: ' + this.x + ' y: ' + this.y + '</b>';
+        }
+    },    */
+    // Additional Plot Options
+    plotOptions:
+    {
+      column: {
+        stacking: null, // Normal bar graph
+        // stacking: "normal", // Stacked bar graph
+        dataLabels: {
+            enabled: false,
+        }
+      }
+    }
+  });
+});
