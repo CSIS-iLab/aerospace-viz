e7d06bffd93d2355346c3415464b11c8f36fbd93
Jacque Schrag <jnschrag@gmail.com>
2018-05-30 10:49:42 -0400

    Update satellite map to use Mapbox map.

--- SatelliteMap/js/app.js
+++ SatelliteMap/js/app.js
@@ -4,7 +4,7 @@ let currentSatellitesNum = 496
 const map = L.map('map').setView([15, 28], 2.5);
 // map.scrollWheelZoom.disable();
 
-L.tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png', {
+L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjhs1kai005bc2sk8vgh79d8d/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
   maxZoom: 18
 }).addTo(map);
 
