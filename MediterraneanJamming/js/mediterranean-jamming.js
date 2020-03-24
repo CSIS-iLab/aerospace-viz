6dacedd9235ea3648f28ec4e6364359c1be5de57
Jacque Schrag <jnschrag@gmail.com>
2020-03-24 13:05:32 -0400

    Update timeline control function

--- MediterraneanJamming/js/mediterranean-jamming.js
+++ MediterraneanJamming/js/mediterranean-jamming.js
@@ -1,5 +1,5 @@
 var basemap = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/ilabmedia/ck7kn54kz0n551ium4z5qzliv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw',
+  "https://api.mapbox.com/styles/v1/ilabmedia/ck7kn54kz0n551ium4z5qzliv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw",
   {}
 );
 
@@ -12,7 +12,7 @@ var map = L.map("map", {
   zoomControl: true,
   layers: [basemap],
   attributionControl: false,
-  addEvent: groupsLoaded,
+  // addEvent: groupsLoaded,
   formatToolbox: formatToolbox
 });
 
@@ -23,7 +23,6 @@ const client = new carto.Client({
 
 const jammedPlacesSource = new carto.source.SQL(
   "SELECT * FROM copy_of_gps_jamming_in_the_mediterranean_sea"
-
 );
 
 const jammedPlacesStyle = new carto.style.CartoCSS(`
@@ -42,10 +41,7 @@ const jammedPlacesLayer = new carto.layer.Layer(
   jammedPlacesSource,
   jammedPlacesStyle,
   {
-    featureOverColumns: [
-      "location_as_reported",
-      "date",
-    ]
+    featureOverColumns: ["location_as_reported", "date"]
   }
 );
 
@@ -60,237 +56,176 @@ const popup = L.popup({ closeButton: true });
 
 jammedPlacesLayer.on(carto.layer.events.FEATURE_CLICKED, createPopup);
 
-let dates = []
-let s
-let e
-let stepVal = 24 * 60 * 60 * 1000
+let dates = [];
+let s;
+let e;
+let stepVal = 24 * 60 * 60 * 1000;
 
-fetch('https://csis.carto.com/api/v2/sql?api_key=3W5HgCXM23QVjB8bFW413w&q=SELECT * FROM copy_of_gps_jamming_in_the_mediterranean_sea')
-  .then((resp) => resp.json())
-  .then((response) => {
+fetch(
+  "https://csis.carto.com/api/v2/sql?api_key=3W5HgCXM23QVjB8bFW413w&q=SELECT * FROM copy_of_gps_jamming_in_the_mediterranean_sea"
+)
+  .then(resp => resp.json())
+  .then(response => {
     response.rows.forEach((row, i) => {
-      let d = row.date.split('/').map(function (value) {
-        return convertType(value)
-      })
-      let date = new Date(d[2], d[0] - 1, d[1])
-      dates.push(date.getTime())
+      let d = row.date.split("/").map(function(value) {
+        return convertType(value);
+      });
+      let date = new Date(d[2], d[0] - 1, d[1]);
+      dates.push(date.getTime());
+    });
+    dates.sort(function(a, b) {
+      return a - b;
+    });
+    len = dates.length;
 
-    })
-    dates.sort(function (a, b) { return a - b })
-    len = dates.length
-    s = dates[0]
-    e = dates[len - 1]
+    console.log(response);
+    console.log(dates);
 
+    timeline.setupTimeline({ start: dates[0], end: dates[len - 1] });
+  });
 
-    // noUiSlider.create(slider, {
-    //   start: [s],
-    //   range: {
-    //     'min': s,
-    //     'max': e,
-    //   },
+var timeline = {
+  el: document.querySelector(".timeline-bar"),
+  controlBtn: document.getElementById("timeline-controls"),
+  playing: false,
+  timer: null,
+  transitionDuration: 1000,
+  end: e,
+  start: s,
+  step: 24 * 60 * 60 * 1000,
+  onChange: function onChange() {
+    now = this.get();
+    Array.from(document.querySelectorAll(".date")).forEach(function(dateEl) {
+      dateEl.innerText = formatDate(new Date(now)).toLocaleDateString(
+        "en-US",
+        dateOptions
+      );
+    });
+    var jams = Array.from(
+      document.querySelectorAll('[class*="jammed-airspace"]')
+    );
+    jams.forEach(function(jam) {
+      var start = parseInt(jam.dataset.start, 10);
+      var end = parseInt(jam.dataset.end, 10);
 
-    //   step: 24 * 60 * 60 * 1000,
-    //   behaviour: 'tap-drag',
-    //   pips: {
-    //     mode: 'range',
-    //     // density: (100 / (e - s)) * stepVal
-    //   }
-    // })
-
-
-    // var dateValue = document.getElementById('event-start')
-
-
-    // dateValue.innerHTML = formatDate(new Date(s))
-
-    // slider.noUiSlider.on('update', function (values) {
-    //   dateValue.innerHTML = formatDate(new Date(+values));
-    // });
-    var timeline = {
-      el: document.querySelector('.timeline-bar'),
-      controlBtn: document.getElementById('timeline-controls'),
-      playing: false,
-      timer: null,
-      transitionDuration: 1000,
-      end: e,
-      start: s,
-      step: 24 * 60 * 60 * 1000,
-      setupTimeline: function setupTimeline(_ref) {
-        var start = _ref.start,
-          end = _ref.end,
-          now = _ref.now,
-          onChange = _ref.onChange
-        this.end = end
-        this.start = start
-
-        noUiSlider.create(this.el, {
-          start: this.start,
-          connect: true,
-          behaviour: 'tap-drag',
-          step: this.step,
-          range: {
-            min: this.start,
-            max: this.end
-          },
-          format: {
-            from: function from(v) {
-              return parseInt(v, 10)
-            },
-            to: function to(v) {
-              return parseInt(v, 10)
-            }
-          },
-          pips: {
-            mode: 'range',
-            density: (100 / (this.end - this.start)) * this.step
-          }
-        })
-        this.el.noUiSlider.set(start)
-        this.setupBtnControls()
-        this.el.noUiSlider.on('update', onChange)
-        this.el.querySelector("[data-value='" + start, "']").innerHTML = formatDate(new Date(
-          start
-        )).toLocaleDateString('en-US', dateOptions)
-        this.el.querySelector("[data-value='" + end, "']").innerHTML = formatDate(new Date(
-          end
-        )).toLocaleDateString('en-US', dateOptions)
-      },
-      setupBtnControls: function setupBtnControls() {
-        this.btnControls.addEventListener('click', function () {
-          if (now == timeline.end) {
-            timeline.el.noUiSlider.set(timeline.start)
-          }
-
-          if (timeline.playing == true) {
-            timeline.stopTimeline()
-            return
-          }
-
-          timeline.timer = setInterval(function () {
-            now += timeline.el.noUiSlider.options.step
-            timeline.el.noUiSlider.set(now)
-          }, timeline.transitionDuration)
-          this.classList.remove('play-btn')
-          this.classList.add('pause-btn')
-          timeline.playing = true
-        })
-      },
-      stopTimeline: function stopTimeline() {
-        clearInterval(timeline.timer)
-        timeline.playing = false
-        timeline.btnControls.classList.remove('pause-btn')
-        timeline.btnControls.classList.add('play-btn')
+      if (now >= start && now <= end) {
+        jam.style.display = "block";
+      } else {
+        jam.style.display = "none";
       }
+    });
+
+    if (now == timeline.end) {
+      timeline.stopTimeline();
+      setTimeout(function() {
+        timeline.el.noUiSlider.set(timeline.start);
+      }, timeline.transitionDuration);
+      jams.forEach(function(jam) {
+        jam.style.display = "none";
+      });
     }
-
-    function formatToolbox(box) {
-      var boxContent =
-        '<div class="separator"></div><section id="scenario"><div class="instruction"><p>Select a military exercise</p><p></p></div>' +
-        Object.keys(scenarioData)
-          .map(function (key) {
-            return (
-              '<button' +
-              (scenario === key ? ' class="active"' : '') +
-              '>' +
-              key +
-              '</button>'
-            )
-          })
-          .join(' ') +
-        '<p class="scenario-description">' +
-        scenarioData[timeline.scenario].description +
-        '</p> <div> </section> <div class="separator"></div> <section id="time"> <div class="indicator"> <p>Signal loss on <span class="date"></span></p> <p><span></span></p> </div> <div class="timeline"> <div class="timeline-controls"> <button class="timeline-btn play-btn"></button> </div> <div class="timeline-container"> <div class="timeline-bar"></div> </div> </div> </section> <!--<p>Click on a point for incident details</p>--> <div class="separator"></div> <section> <ul id="key"> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjZjliYzY1Ii8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">GPS Signal Loss</span></li> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjMTk2Yzk1Ii8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">NATO Activity</span></li> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjZDY2ZTQyIi8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">Russian Military Activity</span></li> </ul> </section> <div class="hidden"></div>'
-
-      box.innerHTML = boxContent
-      scenario = document.querySelector('button.active').innerText
-      timeline.el = document.querySelector('.timeline-bar')
-      timeline.btnControls = document.querySelector('.timeline-btn')
-
-      var m = this
-
-      document
-        .querySelector('#scenario')
-        .addEventListener('click', e => handleSceneClick(e, m))
-    }
-
-    function groupsLoaded() {
-      timeline.end = formatDate(new Date(
-        Math.max.apply(
-          null,
-          endDates.map(function (e) {
-            return new Date(e)
-          })
-        )
-      )).getTime()
-      timeline.start = formatDate(new Date(
-        Math.min.apply(
-          null,
-          startDates.map(function (e) {
-            return new Date(e)
-          })
-        )
-      )).getTime()
-      if (
-        !isNaN(timeline.start) &&
-        !isNaN(timeline.end) &&
-        !timeline.el.noUiSlider
-      ) {
-        now = timeline.start
-        Array.from(document.querySelectorAll('.date')).forEach(function (dateEl) {
-          dateEl.innerText = formatDate(new Date(now)).toLocaleDateString(
-            'en-US',
-            dateOptions
-          )
-        })
-        var timelineOptions = {
-          start: timeline.start,
-          end: timeline.end,
-          now: now,
-          onChange: function onChange() {
-            now = this.get()
-            Array.from(document.querySelectorAll('.date')).forEach(function (
-              dateEl
-            ) {
-              dateEl.innerText = formatDate(new Date(now)).toLocaleDateString(
-                'en-US',
-                dateOptions
-              )
-            })
-            var jams = Array.from(
-              document.querySelectorAll('[class*="jammed-airspace"]')
-            )
-            jams.forEach(function (jam) {
-              var start = parseInt(jam.dataset.start, 10)
-              var end = parseInt(jam.dataset.end, 10)
-
-              if (now >= start && now <= end) {
-                jam.style.display = 'block'
-              } else {
-                jam.style.display = 'none'
-              }
-            })
-
-            if (now == timeline.end) {
-              timeline.stopTimeline()
-              setTimeout(function () {
-                timeline.el.noUiSlider.set(timeline.start)
-              }, timeline.transitionDuration)
-              jams.forEach(function (jam) {
-                jam.style.display = 'none'
-              })
-            }
-          }
+  },
+  setupTimeline: function({ start, end }) {
+    console.log(this);
+    this.start = start;
+    this.end = end;
+    noUiSlider.create(this.el, {
+      start: this.start,
+      connect: true,
+      behaviour: "tap-drag",
+      step: this.step,
+      range: {
+        min: this.start,
+        max: this.end
+      },
+      format: {
+        from: function from(v) {
+          return parseInt(v, 10);
+        },
+        to: function to(v) {
+          return parseInt(v, 10);
         }
-        timeline.setupTimeline(timelineOptions)
+      },
+      pips: {
+        mode: "range",
+        density: (100 / (this.end - this.start)) * this.step
+      }
+    });
+    this.el.noUiSlider.set(this.start);
+    this.setupBtnControls();
+    this.el.noUiSlider.on("update", this.onChange);
+    // this.el.querySelector(
+    //   "[data-value='" + this.start,
+    //   "']"
+    // ).innerHTML = formatDate(new Date(this.start)).toLocaleDateString(
+    //   "en-US",
+    //   dateOptions
+    // );
+    // this.el.querySelector(
+    //   "[data-value='" + this.end,
+    //   "']"
+    // ).innerHTML = formatDate(new Date(this.end)).toLocaleDateString(
+    //   "en-US",
+    //   dateOptions
+    // );
+  },
+  setupBtnControls: function() {
+    this.controlBtn.addEventListener("click", function() {
+      if (now == timeline.end) {
+        timeline.el.noUiSlider.set(timeline.start);
       }
 
-      var first = document.querySelector('[data-start="' + now, '"]')
-      if (first) first.style.display = 'block'
-    }
-  })
+      if (timeline.playing == true) {
+        timeline.stopTimeline();
+        return;
+      }
 
+      timeline.timer = setInterval(function() {
+        now += timeline.el.noUiSlider.options.step;
+        timeline.el.noUiSlider.set(now);
+      }, timeline.transitionDuration);
+      this.classList.remove("play-btn");
+      this.classList.add("pause-btn");
+      timeline.playing = true;
+    });
+  },
+  stopTimeline: function() {
+    clearInterval(timeline.timer);
+    timeline.playing = false;
+    timeline.controlBtn.classList.remove("pause-btn");
+    timeline.controlBtn.classList.add("play-btn");
+  }
+};
 
+function formatToolbox(box) {
+  var boxContent =
+    '<div class="separator"></div><section id="scenario"><div class="instruction"><p>Select a military exercise</p><p></p></div>' +
+    Object.keys(scenarioData)
+      .map(function(key) {
+        return (
+          "<button" +
+          (scenario === key ? ' class="active"' : "") +
+          ">" +
+          key +
+          "</button>"
+        );
+      })
+      .join(" ") +
+    '<p class="scenario-description">' +
+    scenarioData[timeline.scenario].description +
+    '</p> <div> </section> <div class="separator"></div> <section id="time"> <div class="indicator"> <p>Signal loss on <span class="date"></span></p> <p><span></span></p> </div> <div class="timeline"> <div class="timeline-controls"> <button class="timeline-btn play-btn"></button> </div> <div class="timeline-container"> <div class="timeline-bar"></div> </div> </div> </section> <!--<p>Click on a point for incident details</p>--> <div class="separator"></div> <section> <ul id="key"> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjZjliYzY1Ii8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">GPS Signal Loss</span></li> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjMTk2Yzk1Ii8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">NATO Activity</span></li> <li class="label"><span class="colorKey" style="background-image: url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxjaXJjbGUgY3g9IjYiIGN5PSI2IiByPSI1IiBmaWxsPSIjZDY2ZTQyIi8+PC9zdmc+\')"></span><span class="itemText" style="transform: translateY(13.3333%);">Russian Military Activity</span></li> </ul> </section> <div class="hidden"></div>';
 
+  box.innerHTML = boxContent;
+  scenario = document.querySelector("button.active").innerText;
+  timeline.el = document.querySelector(".timeline-bar");
+  timeline.btnControls = document.querySelector(".timeline-btn");
+
+  var m = this;
+
+  document
+    .querySelector("#scenario")
+    .addEventListener("click", e => handleSceneClick(e, m));
+}
 
 function createPopup(event) {
   popup.setLatLng(event.latLng);
@@ -299,10 +234,7 @@ function createPopup(event) {
     var data = event.data;
     var content = "<div>";
 
-    var keys = [
-      "location_as_reported",
-      "date",
-    ];
+    var keys = ["location_as_reported", "date"];
 
     content += `
     <div class="popupHeaderStyle">
@@ -311,7 +243,7 @@ function createPopup(event) {
     <div class="popupEntryStyle">
     ${data.date}
     </div>
-    `
+    `;
     popup.setContent("" + content);
     popup.openOn(map);
   }
@@ -326,44 +258,55 @@ L.control
   )
   .addTo(map);
 
-
 // let slider = document.getElementById("controls")
 
 function convertType(value) {
-  var v = Number(value)
+  var v = Number(value);
   return !isNaN(v)
     ? v
-    : value.toLowerCase() === 'undefined'
-      ? undefined
-      : value.toLowerCase() === 'null'
-        ? null
-        : value.toLowerCase() === 'true'
-          ? true
-          : value.toLowerCase() === 'false'
-            ? false
-            : value
+    : value.toLowerCase() === "undefined"
+    ? undefined
+    : value.toLowerCase() === "null"
+    ? null
+    : value.toLowerCase() === "true"
+    ? true
+    : value.toLowerCase() === "false"
+    ? false
+    : value;
 }
 
 // Create a string representation of the date.
 function formatDate(date) {
   // Create a list of day and month names.
   var weekdays = [
-    "Sunday", "Monday", "Tuesday",
-    "Wednesday", "Thursday", "Friday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
     "Saturday"
   ];
 
   var months = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
   ];
 
   // Append a suffix to dates.
   // Example: 23 => 23rd, 1 => 1st.
   function nth(d) {
-    if (d > 3 && d < 21) return 'th';
+    if (d > 3 && d < 21) return "th";
     switch (d % 10) {
       case 1:
         return "st";
@@ -376,8 +319,14 @@ function formatDate(date) {
     }
   }
 
-  return weekdays[date.getDay()] + ", " +
-    date.getDate() + nth(date.getDate()) + " " +
-    months[date.getMonth()] + " " +
-    date.getFullYear();
+  return (
+    weekdays[date.getDay()] +
+    ", " +
+    date.getDate() +
+    nth(date.getDate()) +
+    " " +
+    months[date.getMonth()] +
+    " " +
+    date.getFullYear()
+  );
 }
