479dc7f74c79d546191d82db9fcb5e7fb07068a7
mariel-delagarza <mariel.code@gmail.com>
2020-06-18 14:10:47 -0400

    Format with Prettier, comment out series names, remove other commented out code

--- VehicleLaunchSFColumn/js/bar.js
+++ VehicleLaunchSFColumn/js/bar.js
@@ -1,164 +1,189 @@
-Highcharts.chart('hcContainer', {
-  data: {  
-      googleSpreadsheetKey: '11gJ3QuXf4_34uDomERHdO3FQNLZ2_6_t8dY_SZKaESo',
-      googleSpreadsheetWorksheet: 1
+Highcharts.chart("hcContainer", {
+  data: {
+    googleSpreadsheetKey: "11gJ3QuXf4_34uDomERHdO3FQNLZ2_6_t8dY_SZKaESo",
+    googleSpreadsheetWorksheet: 1,
   },
   chart: {
-      type: 'column'
+    type: "column",
   },
 
   title: {
-      text: 'Rocket Successes and Failures'
+    text: "Rocket Successes and Failures",
   },
 
   xAxis: {
-      categories: [
-      '2002', 
-      '2003',
-      '2004',
-      '2005',
-      '2006',
-      '2007',
-      '2008',
-      '2009',
-      '2010',
-      '2011',
-      '2012',
-      '2013',
-      '2014',
-      '2015',
-      '2016',
-      '2017',
-      '2018',
-      '2019',
-      '2020']
-},
+    categories: [
+      "2002",
+      "2003",
+      "2004",
+      "2005",
+      "2006",
+      "2007",
+      "2008",
+      "2009",
+      "2010",
+      "2011",
+      "2012",
+      "2013",
+      "2014",
+      "2015",
+      "2016",
+      "2017",
+      "2018",
+      "2019",
+      "2020",
+    ],
+  },
 
-yAxis: {
+  yAxis: {
     allowDecimals: false,
     min: 0,
     title: {
-        text: 'Number of Launches'
+      text: "Number of Launches",
     },
-    reversedStacks: false
-},
+    reversedStacks: false,
+  },
 
-credits: {
-  enabled: true,
-  href: false,
-  text: "CSIS Aerospace Security | Gunter's Space Page, SpaceX, United Launch Alliance"
-},
+  credits: {
+    enabled: true,
+    href: false,
+    text:
+      "CSIS Aerospace Security | Gunter's Space Page, SpaceX, United Launch Alliance",
+  },
 
-plotOptions: {
+  plotOptions: {
     column: {
-        stacking: 'normal'
-    }
-},
+      stacking: "normal",
+    },
+  },
 
-tooltip: {
+  tooltip: {
     shared: true,
     useHTML: true,
-    formatter: function() {
-        let string = ""
+    formatter: function () {
+      let string = "";
 
-        for (i = 0; i < this.points.length; i+=2 ) {
-            //console.log(this.points[i].series.userOptions.stack)
-           // string += '<b>' + this.points[i].series.userOptions.stack + '</b>' + '<br>' + '&emsp;' + this.points[i].series.name.slice(-8) + ': ' + this.points[i].y + '<br>' + 
-            //'&emsp;' + this.points[i+1].series.name.slice(-8) + ': ' + this.points[i+1].y + '</br>'
+      for (i = 0; i < this.points.length; i += 2) {
+        string +=
+          "<tr>" +
+          "<td><b>" +
+          this.points[i].series.userOptions.stack +
+          "</b></td>" +
+          "<td>" +
+          this.points[i].y +
+          "</td>" +
+          "<td>" +
+          this.points[i + 1].y +
+          "</td>" +
+          "</tr>";
+      }
 
-            
-                string += '<tr>' +
-                    '<td><b>' + this.points[i].series.userOptions.stack + '</b></td>' + 
-                    '<td>' + this.points[i].y + '</td>' + 
-                    '<td>' + this.points[i+1].y + '</td>' + 
-                '</tr>' 
-        
-        }
+      return (
+        '<span style="font-size: 14px;"><b>' +
+        this.x +
+        "</b></span>" +
+        "<br>" +
+        "<table>" +
+        "<tr>" +
+        "<th></th>" +
+        "<th> Success </th>" +
+        "<th> Failure </th>" +
+        "</tr>" +
+        string +
+        "</table>"
+      );
+    },
+  },
 
-        return '<span style="font-size: 14px;"><b>' + this.x + '</b></span>' + '<br>' + 
-                
-                '<table>'  + 
-                '<tr>' + 
-                    '<th></th>' + 
-                    '<th> Success </th>' + 
-                    '<th> Failure </th>' + 
-                '</tr>'+ string + 
-                '</table>'
-    }
-},
+  legend: {
+    labelFormatter: function () {
+      if (
+        this.name === "Delta IV Success" ||
+        this.name === "Atlas V Success" ||
+        this.name === "Falcon 9 Success"
+      ) {
+        let name = this.name.split("");
+        let updateName = name.slice(0, 8);
+        finalName = updateName.join("");
+        return finalName;
+      } else if (
+        this.name === "Delta IV Heavy Success" ||
+        this.name === "Falcon 9 Heavy Success"
+      ) {
+        let name = this.name.split("");
+        let updateName = name.slice(0, 14);
+        finalName = updateName.join("");
+        return finalName;
+      } else if (this.name === "Delta IV Failure/Partial Failure") {
+        let name = this.name.split("");
+        let updateName = name.slice(9, 13);
+        finalName = updateName.join("") + "ed Launches";
+        return finalName;
+      }
+    },
+  },
 
-legend: {
-    labelFormatter: function() {
-
-        if ((this.name === "Delta IV Success") || (this.name === "Atlas V Success") || (this.name === "Falcon 9 Success")) {
-            let name = this.name.split("")
-            let updateName = name.slice(0,8)
-            finalName = updateName.join("")
-            return finalName
-        } else if ((this.name === "Delta IV Heavy Success") || (this.name ==="Falcon 9 Heavy Success")) {
-            let name = this.name.split("")
-            let updateName = name.slice(0,14)
-            finalName = updateName.join("")
-            return finalName
-        } else if (this.name === "Delta IV Failure/Partial Failure") {
-            let name = this.name.split("");
-            let updateName = name.slice(9,13);
-            finalName = updateName.join("") + 'ed Launches'
-            return finalName;
-        }
-    }
-},
-
-series: [{
-    name: 'Delta IV',
-    color: '#196C95',
-    stack: 'Delta IV'
-}, {
-    name: 'Delta IV Failure/Partial Failure',
-    color: '#B5BDC1',
-    id: 'Failure',
-    stack: 'Delta IV',
-    legendIndex: 1
-    //showInLegend: false
-}, {
-    name: 'Delta IV Heavy',
-    color: '#5DB6D0',
-    stack: 'Delta IV Heavy'
-}, {
-    //name: 'Delta IV Heavy Failure/Partial Failure',
-    linkedTo: "Failure",
-    color: '#B5BDC1',
-    stack: 'Delta IV Heavy',
-    showInLegend: false
-}, {
-    name: 'Atlas V',
-    color: '#F9BC65',
-    stack: 'Atlas V'
-}, {
-    //name: 'Atlas V Failure/Partial Failure',
-    linkedTo: "Failure",
-    stack: 'Atlas V',
-    color: '#B5BDC1',
-    showInLegend: false
-}, {
-    name: 'Falcon 9',
-    color: '#4F9793',
-    stack: 'Falcon 9'
-}, {
-    //name: 'Falcon 9 Failure/Partial Failure',
-    linkedTo: "Failure",
-    color: '#B5BDC1',
-    stack: 'Falcon 9',
-    showInLegend: false
-}, {
-    name: 'Falcon 9 Heavy',
-    color: '#3E7A82',
-    stack: 'Falcon 9 Heavy'
-}, {
-    //name: 'Falcon 9 Heavy Failure/Partial Failure',
-    linkedTo: "Failure",
-    color: '#B5BDC1',
-    stack: 'Falcon 9 Heavy',
-    showInLegend: false
-}]
+  series: [
+    {
+      //name: "Delta IV",
+      color: "#196C95",
+      stack: "Delta IV",
+    },
+    {
+      //name: "Delta IV Failure/Partial Failure",
+      color: "#B5BDC1",
+      id: "Failure",
+      stack: "Delta IV",
+      legendIndex: 1,
+      //showInLegend: false
+    },
+    {
+      //name: "Delta IV Heavy",
+      color: "#5DB6D0",
+      stack: "Delta IV Heavy",
+    },
+    {
+      //name: 'Delta IV Heavy Failure/Partial Failure',
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Delta IV Heavy",
+      showInLegend: false,
+    },
+    {
+      //name: "Atlas V",
+      color: "#F9BC65",
+      stack: "Atlas V",
+    },
+    {
+      //name: 'Atlas V Failure/Partial Failure',
+      linkedTo: "Failure",
+      stack: "Atlas V",
+      color: "#B5BDC1",
+      showInLegend: false,
+    },
+    {
+      //name: "Falcon 9",
+      color: "#4F9793",
+      stack: "Falcon 9",
+    },
+    {
+      //name: 'Falcon 9 Failure/Partial Failure',
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Falcon 9",
+      showInLegend: false,
+    },
+    {
+      //name: "Falcon 9 Heavy",
+      color: "#3E7A82",
+      stack: "Falcon 9 Heavy",
+    },
+    {
+      //name: 'Falcon 9 Heavy Failure/Partial Failure',
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Falcon 9 Heavy",
+      showInLegend: false,
+    },
+  ],
 });
