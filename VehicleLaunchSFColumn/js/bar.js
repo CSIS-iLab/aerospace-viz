384a5dd604bd15759499565c98f5a98eec3a2de1
Mariel <mariel.code@gmail.com>
2020-06-19 10:56:38 -0400

    Feature space vehicle launch data (#62)
    * Adding Bar and Column Options for Success and Failure Interactive
    
    Both the column and bar code is producing the same graphic
    
    * Manual entry of data/creation of chart
    
    * Second chart draft after feedback from program
    
    * Add credits
    
    * Add test legend formatting
    
    * Update label formatter
    
    * Format tooltip
    
    * Change year font size in tooltip
    
    * Change shade of gray for failure
    
    * Format tooltip as table
    
    * Indent css
    
    * Update sources and title
    
    * Display and link all failure data
    
    * Update failed launch name in legend, move to end of legend
    
    * Tooltip - align text right, add cell padding
    
    * Format with Prettier, comment out series names, remove other commented out code
    
    * Make suggested changes
    
    * Make suggested changes
    
    Co-authored-by: spencer-kaplan <66686377+spencer-kaplan@users.noreply.github.com>

---
+++ VehicleLaunchSFColumn/js/bar.js
@@ -0,0 +1,143 @@
+Highcharts.chart("hcContainer", {
+  data: {
+    googleSpreadsheetKey: "11gJ3QuXf4_34uDomERHdO3FQNLZ2_6_t8dY_SZKaESo",
+    googleSpreadsheetWorksheet: 1,
+  },
+  chart: {
+    type: "column",
+  },
+
+  title: {
+    text: "Rocket Successes and Failures",
+  },
+
+  xAxis: {
+    tickInterval: 1
+  },
+
+  yAxis: {
+    allowDecimals: false,
+    min: 0,
+    title: {
+      text: "Number of Launches",
+    },
+    reversedStacks: false,
+  },
+
+  credits: {
+    enabled: true,
+    href: false,
+    text:
+      "CSIS Aerospace Security | Gunter's Space Page, SpaceX, United Launch Alliance",
+  },
+
+  plotOptions: {
+    column: {
+      stacking: "normal",
+    },
+  },
+
+  tooltip: {
+    shared: true,
+    useHTML: true,
+    formatter: function () {
+      let string = "";
+
+      for (i = 0; i < this.points.length; i += 2) {
+        string +=
+          "<tr>" +
+          "<td><b>" +
+          this.points[i].series.userOptions.stack +
+          "</b></td>" +
+          "<td>" +
+          this.points[i].y +
+          "</td>" +
+          "<td>" +
+          this.points[i + 1].y +
+          "</td>" +
+          "</tr>";
+      }
+
+      return (
+        '<span style="font-size: 14px;"><b>' +
+        this.x +
+        "</b></span>" +
+        "<br>" +
+        "<table>" +
+        "<tr>" +
+        "<th></th>" +
+        "<th> Success </th>" +
+        "<th> Failure </th>" +
+        "</tr>" +
+        string +
+        "</table>"
+      );
+    },
+  },
+
+  legend: {
+    labelFormatter: function () {
+      if (this.name === "Delta IV Failure/Partial Failure") {
+        let name = this.name.split("");
+        let updateName = name.slice(9, 13);
+        finalName = updateName.join("") + "ed Launches";
+        return finalName;
+      } else {
+        return this.name.replace(/success/gi, "");
+      }
+    },
+  },
+
+  series: [
+    {
+      color: "#196C95",
+      stack: "Delta IV",
+    },
+    {
+      color: "#B5BDC1",
+      id: "Failure",
+      stack: "Delta IV",
+      legendIndex: 1,
+    },
+    {
+      color: "#5DB6D0",
+      stack: "Delta IV Heavy",
+    },
+    {
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Delta IV Heavy",
+      showInLegend: false,
+    },
+    {
+      color: "#F9BC65",
+      stack: "Atlas V",
+    },
+    {
+      linkedTo: "Failure",
+      stack: "Atlas V",
+      color: "#B5BDC1",
+      showInLegend: false,
+    },
+    {
+      color: "#4F9793",
+      stack: "Falcon 9",
+    },
+    {
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Falcon 9",
+      showInLegend: false,
+    },
+    {
+      color: "#3E7A82",
+      stack: "Falcon 9 Heavy",
+    },
+    {
+      linkedTo: "Failure",
+      color: "#B5BDC1",
+      stack: "Falcon 9 Heavy",
+      showInLegend: false,
+    },
+  ],
+});
