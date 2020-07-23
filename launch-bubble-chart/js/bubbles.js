436f7125c3ec0383676eafbd3dc374434232d146
mariel-delagarza <mariel.code@gmail.com>
2020-07-23 13:20:53 -0400

    Get bubbles to display properly, remove tooltip formatting from demo code

--- launch-bubble-chart/js/bubbles.js
+++ launch-bubble-chart/js/bubbles.js
@@ -1,73 +1,81 @@
-Highcharts.chart('hcContainer', {
+let allData = [];
 
-  chart: {
-      type: 'bubble',
+Highcharts.data({
+  googleSpreadsheetKey: "1FGdaphIbRjDpXsOdU3omWGRpH5DTImmzWW-H43lLOms",
+  googleSpreadsheetWorksheet: 1,
+  switchRowsAndColumns: true,
+  parsed: function parsed(columns) {
+    columns.splice(0, 2);
+    columns.forEach((row) => {
+      const launchVehicle = row[0];
+      let firstLaunchYear = row[1];
+      let successIncludingSimilarVehicles = row[3];
+      let fy21Cost = row[4];
+
+      allData.push({
+        x: firstLaunchYear,
+        y: fy21Cost,
+        z: successIncludingSimilarVehicles,
+      });
+    });
+    renderChart(allData);
+  },
+});
+
+function renderChart(allData) {
+  Highcharts.chart("hcContainer", {
+    chart: {
+      type: "bubble",
       plotBorderWidth: 1,
-      zoomType: 'xy'
-  },
+      zoomType: "xy",
+    },
 
-  legend: {
-      enabled: false
-  },
+    legend: {
+      enabled: false,
+    },
 
-  title: {
-      text: 'Comparing Cost of Launch to Low-Earth Orbit for Successful Orbital Launch Vehicles'
-  },
+    title: {
+      text:
+        "Comparing Cost of Launch to Low-Earth Orbit for Successful Orbital Launch Vehicles",
+    },
 
-  subtitle: {
-      text: "Bubble sizes correspond to the vehicle's number of successful launches"
-  },
+    subtitle: {
+      text:
+        "Bubble sizes correspond to the vehicle's number of successful launches",
+    },
 
-  xAxis: {
+    xAxis: {
       gridLineWidth: 1,
       title: {
-          text: 'Year of First Successful Orbital Launch'
+        text: "Year of First Successful Orbital Launch",
       },
-  },
+    },
 
-  yAxis: {
+    yAxis: {
       title: {
-          text: '$K / kg (FY21 dollars)'
+        text: "$K / kg (FY21 dollars)",
       },
-  },
-  tooltip: {
+    },
+    tooltip: {
       useHTML: true,
-      headerFormat: '<table>',
-      pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
-          '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
-          '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
-          '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
-      footerFormat: '</table>',
-      followPointer: true
-  },
+      headerFormat: "<table>",
+      footerFormat: "</table>",
+      followPointer: true,
+    },
 
-  plotOptions: {
+    plotOptions: {
       series: {
-          dataLabels: {
-              enabled: true,
-              format: '{point.name}'
-          }
-      }
-  },
-
-  series: [{
-      data: [
-          { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
-          { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
-          { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
-          { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
-          { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
-          { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
-          { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
-          { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
-          { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
-          { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
-          { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
-          { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
-          { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
-          { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
-          { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
-      ]
-  }]
-
-});
+        dataLabels: {
+          enabled: true,
+          format: "{point.name}",
+        },
+      },
+    },
+    series: [
+      {
+        type: "bubble",
+        data: allData,
+      },
+    ],
+  });
+}
