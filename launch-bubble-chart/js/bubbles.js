21f105e95685691d458515ebc642333f986ce3d2
mariel-delagarza <mariel.code@gmail.com>
2020-07-27 13:32:59 -0400

    Functionining chart - switch between FY21, Then-Year Dollars. Slow needs to be refactored.

--- launch-bubble-chart/js/bubbles.js
+++ launch-bubble-chart/js/bubbles.js
@@ -1,4 +1,7 @@
-let allData = [];
+let fy21Data = [];
+let thenYearData = [];
+let allData = [fy21Data, thenYearData];
+let datasets = ["FY21 Dollars", "Then-Year Dollars"]
 
 Highcharts.data({
   googleSpreadsheetKey: "1FGdaphIbRjDpXsOdU3omWGRpH5DTImmzWW-H43lLOms",
@@ -9,20 +12,54 @@ Highcharts.data({
     columns.forEach((row) => {
       const launchVehicle = row[0];
       let firstLaunchYear = row[1];
+      let successfulLaunches = row[2];
       let successIncludingSimilarVehicles = row[3];
-      let fy21Cost = row[4];
+      let fy21CostPerKg = row[4];
+      let fy21TotalLaunchCost = row[5];
+      let thenYearCostPerKg = row[6];
+      let thenYearLaunchCost = row[7];
+      let launchClass = row[8];
+      let country = row[9];
+      let similarVehicles = row[10];
+      let source = row[11];
 
-      allData.push({
+      
+
+      if (successfulLaunches != null) {
+      fy21Data.push({
         x: firstLaunchYear,
-        y: fy21Cost,
+        y: fy21CostPerKg,
         z: successIncludingSimilarVehicles,
+        fy21TotalLaunchCost: fy21TotalLaunchCost,
+        launchVehicle: launchVehicle,
+        successfulLaunches: successfulLaunches,
+        launchClass: launchClass,
+        country: country,
+        similarVehicles: similarVehicles,
+        source: source,
+        yAxisTitle: "$K / kg (FY21 Dollars)"
       });
+
+      thenYearData.push({
+        x: firstLaunchYear,
+        y: thenYearCostPerKg,
+        z: successIncludingSimilarVehicles,
+        thenYearLaunchCost: thenYearLaunchCost,
+        launchVehicle: launchVehicle,
+        successfulLaunches: successfulLaunches,
+        launchClass: launchClass,
+        country: country,
+        similarVehicles: similarVehicles,
+        source: source,
+        yAxisTitle: "$K / kg (Then-Year Dollars)"
+      });
+    }
     });
-    renderChart(allData);
+    renderChart(allData[0], datasets[0]);
   },
 });
 
-function renderChart(allData) {
+function renderChart(fy21Data, dataset) {
   Highcharts.chart("hcContainer", {
     chart: {
       type: "bubble",
@@ -51,14 +88,112 @@ function renderChart(allData) {
       },
     },
 
-    yAxis: {
+    yAxis: [{
       title: {
-        text: "$K / kg (FY21 dollars)",
+        text: "$K / kg (" + dataset + ")",
       },
-    },
+    }],
     tooltip: {
       useHTML: true,
-      headerFormat: "<table>",
+      formatter: function() {
+
+        let launchVehicle = this.point.launchVehicle
+        let firstSuccessfulLaunch = this.point.x
+        let successfulLaunches = this.point.successfulLaunches
+        let successIncludingSimilarVehicles = this.point.z
+        let fy21CostPerKg = this.point.y
+        let fy21TotalLaunchCost = this.point.fy21TotalLaunchCost
+        let launchClass = this.point.launchClass
+        let country = this.point.country
+        let similarVehicles = this.point.similarVehicles
+        let source = this.point.source
+
+        if (similarVehicles == null) {
+          return (
+            '<span style="font-size: 14px; width: 500px;"><b>' + launchVehicle + "</b></span>" +
+            "<br>" +
+            "<table>" +
+            "<tr>" +
+              "<td class='columnOne'><b>First Successful Launch</b></td>" + 
+              "<td class='columnTwo'>" + firstSuccessfulLaunch + "</td>" + 
+            "</tr>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Successful Launches</b></td>" + 
+              "<td class='columnTwo'>" + successfulLaunches + "</td>" +
+            "</tr>" + 
+            "<tr>"+ 
+              "<td class='columnOne'><b>Successful Launches Including Similar Vehicles</b></td>" + 
+              "<td class='columnTwo'>" + successIncludingSimilarVehicles + "</td>" + 
+            "</tr>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Launch Cost Per Kilogram ($/kg)</b></td>" + 
+              "<td class='columnTwo'>" + fy21CostPerKg + "</td>" + 
+            "</tr>" + 
+              "<td class='columnOne'><b>Total Launch Cost ($M)</b></td>" + 
+              "<td class='columnTwo'>" + fy21TotalLaunchCost + "</td>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Launch Class</b></td>" + 
+              "<td class='columnTwo'>" + launchClass + "</td>" +
+            "</tr>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Country</b></td>" + 
+              "<td class='columnTwo'>" + country + "</td>" +
+            "</tr>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Similar Vehicles (Number of Successful Launches)</b></td>" + 
+              "<td class='columnTwo'></td>" + 
+            "</tr>" + 
+            "<tr>" + 
+              "<td class='columnOne'><b>Source</b></td>" +
+              "<td class='columnTwo'>" + source + "</td>" +
+            "</tr>"
+            + "</table>"
+          )
+        } else{
+            return (
+              '<span style="font-size: 14px;"><b>' +
+              launchVehicle +
+              "</b></span>" +
+              "<br>" +
+              "<table>" +
+              "<tr>" +
+                "<td class='columnOne'><b>First Successful Launch</b></td>" + 
+                "<td class='columnTwo'>" + firstSuccessfulLaunch + "</td>" + 
+              "</tr>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Successful Launches</b></td>" + 
+                "<td class='columnTwo'>" + successfulLaunches + "</td>" +
+              "</tr>" + 
+              "<tr>"+ 
+                "<td class='columnOne'><b>Successful Launches Including Similar Vehicles</b></td>" + 
+                "<td class='columnTwo'>" + successIncludingSimilarVehicles + "</td>" + 
+              "</tr>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Launch Cost Per Kilogram ($/kg)</b></td>" + 
+                "<td class='columnTwo'>" + fy21CostPerKg + "</td>" + 
+              "</tr>" + 
+                "<td class='columnOne'><b>Total Launch Cost ($M)</b></td>" + 
+                "<td class='columnTwo'>" + fy21TotalLaunchCost + "</td>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Launch Class</b></td>" + 
+                "<td class='columnTwo'>" + launchClass + "</td>" +
+              "</tr>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Country</b></td>" + 
+                "<td class='columnTwo'>" + country + "</td>" +
+              "</tr>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Similar Vehicles (Number of Successful Launches)</b></td>" + 
+                "<td class='columnTwo'>" + similarVehicles + "</td>" + 
+              "</tr>" + 
+              "<tr>" + 
+                "<td class='columnOne'><b>Source</b></td>" +
+                "<td class='columnTwo'>" + source + "</td>" +
+              "</tr>"
+              + "</table>"
+            );        
+          }
+      },
       footerFormat: "</table>",
       followPointer: true,
     },
@@ -74,8 +209,16 @@ function renderChart(allData) {
     series: [
       {
         type: "bubble",
-        data: allData,
+        data: fy21Data,
       },
     ],
   });
+}
+
+function populateSelect() {
+  $(".dropdown").on("change", function() {
+    var chart = $("#hcContainer").highcharts();
+      chart.destroy();
+      renderChart(allData[this.value], datasets[this.value]);
+  })
 }
